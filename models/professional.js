987ec322f40f3cffccdf267bb048ae9055788c63
Anna <anna.radawska@gmail.com>
2018-02-17 16:01:33 +0100

    professional model

--- models/professional.js
+++ models/professional.js
@@ -1,14 +1,50 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+const Education = new Schema({
+  title: String,
+  university: String,
+  graduation: Date,
+  decription: String,
+});
+
+const Experience = new Schema({
+  title: String,
+  company: String,
+  duration: {
+    from: Date,
+    to: Date,
+  },
+  current: Boolean,
+  decription: String,
+});
+
+const Contact = new Schema({
+  email: String,
+  linkedIn: String,
+  landline: Number,
+  mobile: Number,
+  address: {
+    city: String,
+    street: String,
+    zip: Number,
+    country: String,
+  },
+});
+
 const professionalSchema = new Schema({
   username: String,
   password: String,
   role: String,
+  profession: Array,
+  education: [Education],
+  experience: [Experience],
+  contact: [Contact],
 }, {
   timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
 });
 
-const Professional = mongoose.model('Professional', professionalSchema);
+const Professional = mongoose.model('Professionals', professionalSchema);
 
 module.exports = Professional;
