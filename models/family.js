a4f73b7918b877a8aae5aa51adb75124cafb8231
luis-sanfelices <l.fernando.sanfelices@gmail.com>
2018-02-24 15:34:37 +0100

    familily form

--- models/family.js
+++ models/family.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const personalInformation = new Schema({
+  name: String,
+  lastName: String,
+  gender: Number,
+  birthdate: Date,
+  email: String,
+  phone: String,
+  address: String,
+});
+
 const familySchema = new Schema({
   username: String,
   password: String,
+  personalInformation: [personalInformation],
   role: String,
 }, {
   timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
--- routes/profiles/profile.js
+++ routes/profiles/profile.js
@@ -4,13 +4,35 @@ const ensureLogin = require('connect-ensure-login');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
+const Family = require('../../models/family');
+
 
 router.get('/family', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('profiles/family', { user: req.user });
+  if (req.user.role === 'family') {
+    res.render('profiles/family', { user: req.user });
+  } else {
+    res.redirect('professional');
+  }
+});
+
+router.post('/family/:id', (req, res) => {
+  console.log(req.body);
+  const { profession } = req.body;
+  const { id } = req.params;
+  Family.findByIdAndUpdate(
+    { _id : id }, {
+      $push: { profession } },
+    { new: true },
+    (err, family) => console.log(err, family )//res.redirect('/profiles/professional')
+  );
 });
 
 router.get('/professional', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('profiles/professional', { user: req.user });
+  if (req.user.role === 'professional') {
+    res.render('profiles/professional', { user: req.user });
+  } else {
+    res.redirect('family');
+  }
 });
 
 module.exports = router;
